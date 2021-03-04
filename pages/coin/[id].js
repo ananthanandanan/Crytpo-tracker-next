0b49a7ff402197cc73fcff711274b82270f1e0fb
ananthanandanan <ananthanandanan@gmail.com>
2021-03-04 18:07:37 +0530

    simple crypto tracker

---
+++ pages/coin/[id].js
@@ -0,0 +1,13 @@
+import Layout from '../../components/Layout'
+
+import React from 'react'
+
+const Coin = () => {
+    return (
+        <Layout>
+            <h1>hi</h1>
+        </Layout>
+    )
+}
+
+export default Coin
